02320f4327d27e3a42e03188af5ae620209068da
sana faraji <sanafaraji82@gmail.com>
2023-12-05 19:01:28 +0330

    comment_review commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const body_parser =require('body-parser')
 const morgan=require('morgan')
 const cors=require('cors')
 const helmet=require('helmet')
-const xss=require('xss')
+
 const notfound=require('./handlers/NotFoundHandler')
 const errorhandler=require('./handlers/errorhandler')
 const commentroutes=require('./routers/commentrouter')
@@ -15,11 +15,24 @@ const reviewroutes=require('./routers/reviewrouter')
 
 const PORT=6001
 
-
+const corsOptions = {
+  origin: 'http://localhost:6001/',
+  credentials: true,
+  
+}
+app.set('trust proxy', 1);
 app.use(morgan('tiny'))  
 app.use(helmet()) 
-app.use(cors())  
 
+app.use(cors(corsOptions))  
+app.options('*',cors())
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', "http://localhost:6001");
+  res.header('Access-Control-Allow-Headers', true);
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+});
 //app.use(express.json({extended:false}))
 app.use(body_parser.json())
 app.use(body_parser.urlencoded({extended:true}))
